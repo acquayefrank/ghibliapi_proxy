44674ef479b7e4cdb6fb56e9c7b2f0115c033fb2
Frank Acquaye <acquayefrank@gmail.com>
2020-06-22 19:27:39 +0300

    fixed mongo

--- app.py
+++ app.py
@@ -43,7 +43,9 @@ def pg_films(request):
 
 @app.route('/api/v1/films/<film_id>/')
 def pg_film(request, film_id):
-    data = client.DB.films.find_one({'film_id': film_id})
+    db = client.films
+    collection = db.films
+    data = collection.find_one({'film_id': film_id})
     film = get_ghibli_film(film_id)
     film['r_name'] = data['r_name'] #.encode(encoding = 'UTF-8',errors = 'strict')
     response = sjson.dumps(film) #, ensure_ascii=False).encode('utf8')
